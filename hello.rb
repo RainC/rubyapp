39897000460fcf15138c6c1fca27046cde01f52a
RainC <support@rainclab.net>
2018-05-20 19:54:41 +0900

    test
    Signed-off-by: RainC <support@rainclab.net>

--- hello.rb
+++ hello.rb
@@ -8,6 +8,10 @@ get '/surface' do
   "is microsoft "
 end
 
+get '/test-something' do
+  "test-something-is-cool"
+end
+
 get '/routing-something' do
   "<h1>this is working</h1><h1>rubycs.rainclab.net</h1><h1>rainclab.net</h1>"
 end
