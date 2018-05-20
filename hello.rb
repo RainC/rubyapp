7d70d282d522a5af663ee82e9fb1a4e2027ceede
RainC <support@rainclab.net>
2018-05-20 20:01:54 +0900

    v2
    Signed-off-by: RainC <support@rainclab.net>

--- hello.rb
+++ hello.rb
@@ -1,17 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
+  "v2 deployed"
 end
 
-get '/surface' do
-  "is microsoft "
-end
-
-get '/test-something' do
-  "test-something-is-cool"
-end
-
-get '/routing-something' do
-  "<h1>this is working</h1><h1>rubycs.rainclab.net</h1><h1>rainclab.net</h1>"
-end
+ 
